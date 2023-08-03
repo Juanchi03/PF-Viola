fa266b7be3a86ffbfd60399c38642002949b2d08
Juanchi <juanchiviola@gmail.com>
2023-08-03 19:38:19 -0300

    storage

--- javascript/java.js
+++ javascript/java.js
@@ -1,18 +1,18 @@
 function logeoSesion() {
-  // Agregar evento al botón "Iniciar sesión"
+  
   const btnIniciarSesion = document.querySelector('#loginButton');
   btnIniciarSesion.addEventListener('click', function(event) {
     event.preventDefault();
 
-    // Obtener los valores ingresados en los campos
+    
     const email = document.querySelector('#loginEmail').value;
     const password = document.querySelector('#loginPassword').value;
 
-    // Guardar los valores en el LocalStorage
+    
     localStorage.setItem('email', JSON.stringify(email));
     localStorage.setItem('password', JSON.stringify(password));
 
-    // Redirigir a la página "Intranet.html" después de 1 segundo (1000 ms)
+    
     setTimeout(function() {
       window.location.href = "Intranet.html";
     }, 1000);
