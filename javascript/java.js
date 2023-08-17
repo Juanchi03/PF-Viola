1d46652adcbb5ae9fdc8a76e9b951e9a986070c2
Juanchi <juanchiviola@gmail.com>
2023-08-17 17:52:10 -0300

    jj

--- javascript/java.js
+++ javascript/java.js
@@ -1,25 +1,85 @@
-function logeoSesion() {
+// function logeoSesion() {
   
-  const btnIniciarSesion = document.querySelector('#loginButton');
-  btnIniciarSesion.addEventListener('click', function(event) {
-    event.preventDefault();
+  //const btnIniciarSesion = document.querySelector('#loginButton');
+  //btnIniciarSesion.addEventListener('click', function(event) {
+    //event.preventDefault();
 
     
+    //const email = document.querySelector('#loginEmail').value;
+    //const password = document.querySelector('#loginPassword').value;
+
+    
+    //localStorage.setItem('email', JSON.stringify(email));
+    //localStorage.setItem('password', JSON.stringify(password));
+
+    
+    //setTimeout(function() {
+      //window.location.href = "Intranet.html";
+    //}, 1000);
+  //});
+//}
+
+//document.addEventListener("DOMContentLoaded", function() {
+  //logeoSesion();
+//}); //
+
+document.addEventListener("DOMContentLoaded", function() {
+  const btnIniciarSesion = document.querySelector('#loginButton');
+
+  btnIniciarSesion.addEventListener('click', async function(event) {
+    event.preventDefault();
+
     const email = document.querySelector('#loginEmail').value;
     const password = document.querySelector('#loginPassword').value;
 
-    
-    localStorage.setItem('email', JSON.stringify(email));
-    localStorage.setItem('password', JSON.stringify(password));
+    if (!email || !password) {
+      Swal.fire(
+  'Datos Incorrectos',
+  'Reintentar',
+  'question'
+)
+      return;
+    }
 
-    
-    setTimeout(function() {
-      window.location.href = "Intranet.html";
-    }, 1000);
+    try {
+      const userData = await cargarUsuariosDesdeJSON(); // Cargar datos desde el JSON
+      
+      const authenticated = verificarCredenciales(userData.usuarios, email, password);
+      
+      if (authenticated) {
+        localStorage.setItem('email', JSON.stringify(email));
+        
+        setTimeout(function() {
+          window.location.href = "Intranet.html";
+        }, 1000);
+      } else {
+        Swal.fire(
+  'Datos Incorrectos',
+  'Reintentar',
+  'question'
+)
+      }
+    } catch (error) {
+      console.error('Error al cargar usuarios desde JSON:', error);
+    }
   });
+
+  async function cargarUsuariosDesdeJSON() {
+    try {
+      const response = await fetch('data.json');
+      
+      if (response.ok) {
+        return await response.json();
+      } else {
+        throw new Error('Error en la carga del JSON');
+      }
+    } catch (error) {
+      throw new Error('Error al cargar JSON:', error);
+    }
+  }
+
+function verificarCredenciales(usuarios, email, password) {
+  const usuarioEncontrado = usuarios.find(usuario => usuario.email === email);
+  return usuarioEncontrado && usuarioEncontrado.password === password;
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-  logeoSesion();
 });
-
