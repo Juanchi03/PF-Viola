2a66cf567afdff795e1e04406c44de849eed1784
Juanchi <juanchiviola@gmail.com>
2023-08-17 17:59:13 -0300

    jj

--- javascript/java.js
+++ javascript/java.js
@@ -1,27 +1,4 @@
-// function logeoSesion() {
-  
-  //const btnIniciarSesion = document.querySelector('#loginButton');
-  //btnIniciarSesion.addEventListener('click', function(event) {
-    //event.preventDefault();
 
-    
-    //const email = document.querySelector('#loginEmail').value;
-    //const password = document.querySelector('#loginPassword').value;
-
-    
-    //localStorage.setItem('email', JSON.stringify(email));
-    //localStorage.setItem('password', JSON.stringify(password));
-
-    
-    //setTimeout(function() {
-      //window.location.href = "Intranet.html";
-    //}, 1000);
-  //});
-//}
-
-//document.addEventListener("DOMContentLoaded", function() {
-  //logeoSesion();
-//}); //
 
 document.addEventListener("DOMContentLoaded", function() {
   const btnIniciarSesion = document.querySelector('#loginButton');
